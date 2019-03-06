23f7ddf5320efdb41bbce79a4f18af4c97f19bd0
3yebMB <3yebmb@gmail.com>
2019-03-07 01:06:09 +0500

    add wallpaper to Desktop and Android configuration

--- desktop/src/ru/geekbrains/desktop/DesktopLauncher.java
+++ desktop/src/ru/geekbrains/desktop/DesktopLauncher.java
@@ -7,6 +7,8 @@ import ru.geekbrains.StarGame;
 public class DesktopLauncher {
 	public static void main (String[] arg) {
 		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		config.title = "StarTrack Game";
+		config.resizable = false;
 		new LwjglApplication(new StarGame(), config);
 	}
 }

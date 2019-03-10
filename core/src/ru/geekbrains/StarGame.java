3ac1e160cbc3b17caa4fa66b145a2e68974dc48b
3yebMB <3yebmb@gmail.com>
2019-03-10 15:31:00 +0500

    using vectors

--- core/src/ru/geekbrains/StarGame.java
+++ core/src/ru/geekbrains/StarGame.java
@@ -16,6 +16,37 @@ public class StarGame extends ApplicationAdapter {
 	public void create () {
 		batch = new SpriteBatch();
 		img = new Texture("wallpaper.jpg");
+		
+		Vector2 v1 = new Vector2(1, 3);
+		Vector2 v2 = new Vector2();
+		v2.set(0, -1);
+
+		Vector2 v3 = v1.cpy().add(v2);
+		System.out.println("add v1.x = " + v1.x + " v1.y = " + v1.y);
+		System.out.println("add v3.x = " + v3.x + " v3.y = " + v3.y);
+
+		v1.set(5,6);
+		v2.set(2,1);
+		v1.sub(v2);
+		System.out.println("sub v1.x = " + v1.x + " v1.y = " + v1.y);
+
+		System.out.println("v1 len = " + v1.len());
+		v1.scl(0.5f);
+		System.out.println("scl v1.x = " + v1.x + " v1.y = " + v1.y);
+		System.out.println("v1 len = " + v1.len());
+		v1.nor();
+		System.out.println("nor v1.x = " + v1.x + " v1.y = " + v1.y);
+		System.out.println("v1 len = " + v1.len());
+		v1.setLength(1.5811388f);
+		System.out.println("v1 len = " + v1.len());
+		v1.nor();
+		System.out.println("v1 len = " + v1.len());
+
+		v1.set(1,1);
+		v2.set(-1,1);
+		v1.nor();
+		v2.nor();
+		System.out.println(Math.acos(v1.dot(v2)));
 	}
 
 	@Override

c6225da017cfd637b2f3172a82976fa36e639c87
3yebMB <3yebmb@gmail.com>
2019-03-21 15:49:23 +0500

    add logic in EnemyShip class

--- core/src/ru/geekbrains/sprite/EnemyShip.java
+++ core/src/ru/geekbrains/sprite/EnemyShip.java
@@ -1,6 +1,7 @@
 package ru.geekbrains.sprite;
 
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.graphics.g2d.TextureAtlas;
 import com.badlogic.gdx.graphics.g2d.TextureRegion;
 import com.badlogic.gdx.math.Vector2;
 
@@ -9,18 +10,26 @@ import ru.geekbrains.math.Rect;
 
 public class EnemyShip extends Sprite {
 
+    private TextureAtlas atlas;
     private Rect worldBounds;
-    private Vector2 v;
+    private Vector2 v, vbull;
     private int damage;
-    private TextureRegion skin;
+    private boolean isDestroyed;
+//    private TextureRegion skin;
+    private int live;
 
-    public EnemyShip(TextureRegion region, int rows, int cols, int frames) {
-        super(region, rows, cols, frames);
+    public EnemyShip(TextureAtlas atlas) {
+        super(atlas.findRegion("main_ship"), 1, 2, 2);
+        this.atlas = atlas;
+        setHeightProportion(0.15f);
+        this.v = new Vector2();
+        this.regions = new TextureRegion[2];
     }
 
     @Override
     public void update(float delta) {
-        super.update(delta);
+        pos.mulAdd(v, delta);
+        if (isOutside(worldBounds)) destroyd();
     }
 
     @Override
@@ -30,6 +39,37 @@ public class EnemyShip extends Sprite {
 
     @Override
     public void resize(Rect worldBounds) {
-        super.resize(worldBounds);
+//        super.resize(worldBounds);
+        this.worldBounds = worldBounds;
+    }
+
+    public void destroyd() {
+        isDestroyed = true;
+    }
+
+    public void unDestroy() {
+        isDestroyed = false;
+    }
+
+    public int getDamage() {
+        return damage;
+    }
+
+    public void setDamage(int damage) {
+        this.damage = damage;
+    }
+
+    public void shoot() {}
+
+    private void moveRight() {
+//        v.set(v0);
+    }
+
+    private void moveLeft() {
+//        v.set(v0).rotate(180);
+    }
+
+    private void stop() {
+        v.setZero();
     }
 }
